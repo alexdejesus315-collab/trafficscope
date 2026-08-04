f371bb14ac1bd09239c0deb5f164f6c573d157c1
Amonique <alexdejesus315@gmail.com>
2026-08-04 19:07:49 +0100

    Update: envio geral do projeto

--- server.ts
+++ server.ts
@@ -242,7 +242,7 @@ async function getDomainDataWithCache(requestedDomain: string): Promise<any> {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
 app.post(
   '/api/webhooks/paddle',
